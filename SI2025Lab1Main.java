0a7599012b54f9e5b51d00d4bb3847643651deb4
Ivana Nedevska <ivana.nedevska04@gmail.com>
2025-03-23 18:22:27 +0100

    Find all completed tasks

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -129,9 +129,15 @@ class TaskManager {
 
     // 2. Find all completed tasks
     public List<Task> getCompletedTasks() {
-        // TODO: Implement logic to return completed tasks
-        return new ArrayList<>();
+        ArrayList<Task> completedTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            if (task.isCompleted) {
+                completedTasks.add(task);
+            }
+        }
+        return completedTasks;
     }
+
 
     // 3. List tasks sorted by name
     public void sortTasksByName() {
@@ -170,6 +176,14 @@ class TaskManager {
     public void markCategoryCompleted(String category) {
         // TODO: Implement bulk completion logic
     }
+ public void markTaskCompleted(String name) {
+        for (Task task : tasks) {
+            if (task.name.equals(name)) {
+                task.markAsCompleted();
+                break;
+            }
+        }
+    }
 }
 
 public class SI2025Lab1Main {
@@ -178,7 +192,10 @@ public class SI2025Lab1Main {
         manager.addTask("Write report", Priority.HIGH, "Work");
         manager.addTask("Submit assignment", Priority.MEDIUM, "School");
         manager.addTask("Buy groceries", Priority.LOW, "Personal");
-
+ System.out.println("\nCompleted Tasks:");
+        for (Task task : menager.getCompletedTasks()) {
+            System.out.println(task);
+        }
         // MISSING: Calls to the new methods that will be implemented
 
         manager.printTasks();
